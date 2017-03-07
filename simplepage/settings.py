7908d8da553e94ee3928af4a30b09efc86cf7575
mottaman <Apple@MacBook-Pro-de-Apple-10.local>
2017-03-07 17:31:58 -0600

    Change on settings

--- simplepage/settings.py
+++ simplepage/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'dibgd4&x(!0v#to=qgc*+@tn2l(!0dk1w6y(hdm$f(m7ihe5(%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://pageonpython-webpage.44fs.preview.openshiftapps.com/']
+ALLOWED_HOSTS = [
+]
 
 
 # Application definition
