ec8e1127a7ab45f25ed3a2dd40991b97c3e202eb
mottaman <Apple@MacBook-Pro-de-Apple-10.local>
2017-03-07 16:14:23 -0600

    venv deleted

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/mottaman/EXPERIMENTOS/openshift/django/simplepage/venv/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
